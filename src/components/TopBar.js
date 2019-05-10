42383d6df9698b5426dcbca8822b076590943fca
markhilkert <mark.hilkert@gmail.com>
2019-05-09 20:33:01 -0500

    updated some links, but left others as router-link because they'll be easier to find/replace

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,7 +3,7 @@ import React from "react"
 const TopBar = () => 
   <div className="topbar-mobile">
     <div className="logo">
-      <router-link to="/"><img src="/images/logo.png" alt="Mark. Est. 1985" className style={{height: '44px'}} /></router-link>
+      <a href="/"><img src="/images/logo.png" alt="Mark. Est. 1985" className style={{height: '44px'}} /></a>
       <button className="button-menu-mobile">
         <i className="mdi mdi-menu" />
       </button>
